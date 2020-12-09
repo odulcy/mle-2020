c4543eebee0d1d48d180220d0fd8312a1b08f188
Olivier Dulcy <olivier.dulcy@gmail.com>
2020-12-09 11:13:24 +0100

    Add docstrings and simplify get_movie_id

--- content_based_filtering/helpers/movies.py
+++ content_based_filtering/helpers/movies.py
@@ -1,18 +1,34 @@
 def get_movie_id(movies, title, year=None):
+    """Retrieve a list of movie id based on a
+    title and a year (optional)
+    Args:
+        movies (Pandas DataFrame)
+        title (string)
+        year (int, optional). Default to None
+    Returns:
+        Pandas Index
+    """
     res = movies[movies['title'] == title]
     if year:
         res = res[res['year'] == year]
-
-    if len(res) > 1:
-        print("Ambiguous: found")
-        print(f"{res['title']} {res['year']}")
-    elif len(res) == 0:
-        print('not found')
-    else:
-        return res.index[0]
+    return res.index
 
 def get_movie_name(movies, index):
+    """Get a movie by index and return its title
+    Args:
+        movies (Pandas DataFrame)
+        index (int)
+    Returns:
+        String : title
+    """
     return movies.iloc[index].title
 
 def get_movie_year(movies, index):
+    """Get a movie by index and return its year
+    Args:
+        movies (Pandas DataFrame)
+        index (int)
+    Returns:
+        Int : year
+    """
     return movies.iloc[index].year
