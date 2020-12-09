434563200c2891beb5187cfb55808e00cd9efa73
Olivier Dulcy <olivier.dulcy@gmail.com>
2020-12-09 10:22:36 +0100

    Update dataloader docstrings. Add set_index and modify type.

--- content_based_filtering/helpers/dataloader.py
+++ content_based_filtering/helpers/dataloader.py
@@ -1,41 +1,43 @@
 import pandas as pd
 
-
-def load_users(path="data/users.csv"):
-    """Load user database from a csv file.
+def load_users(path):
+    """Load users database from a csv file.
     
     Args:
         path (string, path object, or file-like object).
             Defaults to `data/users.csv`
     
     Returns:
-        Pandas DataFrame : user database
+        Pandas DataFrame : users database
     """
     users = pd.read_csv(path)
+    users.set_index("user_id", inplace=True)
     return users
 
-def load_movies(path="data/movies.csv"):
-    """Load user database from a csv file.
+def load_movies(path):
+    """Load movies database from a csv file.
     
     Args:
         path (string, path object, or file-like object).
             Defaults to `data/movies.csv`
     
     Returns:
-        Pandas DataFrame : user database
+        Pandas DataFrame : movies database
     """
     movies = pd.read_csv(path)
+    movies.loc[:, movies.dtypes == 'float64'] = movies.loc[:, movies.dtypes == 'float64'].astype('int8')
+    movies.set_index("movie_id", inplace=True)
     return movies
 
-def load_ratings(path="data/ratings.csv"):
-    """Load user database from a csv file.
+def load_ratings(path):
+    """Load ratings database from a csv file.
     
     Args:
         path (string, path object, or file-like object).
             Defaults to `data/ratings.csv`
     
     Returns:
-        Pandas DataFrame : user database
+        Pandas DataFrame : ratings database
     """
     ratings = pd.read_csv(path)
     return ratings
